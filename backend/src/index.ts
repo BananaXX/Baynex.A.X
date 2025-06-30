c71aade3da8776e1aa9b97e363e95713c2e1c583
BananaXX <60035896+BananaXX@users.noreply.github.com>
2025-06-30 18:23:08 +0300

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,5 @@
+// backend/src/index.ts
+
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
@@ -9,7 +11,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Setup custom routes
 setupRoutes(app);
 
 const PORT = process.env.PORT || 5000;
